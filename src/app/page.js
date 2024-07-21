c4853f692a901eb899f76fe18c3a67fc22985ac9
Sumitsh28 <sumit.off28@gmail.com>
2024-07-21 10:38:48 +0530

    ssr

--- src/app/page.js
+++ src/app/page.js
@@ -61,7 +61,9 @@ export default function Home() {
       }
     };
 
-    getUserDetails();
+    if (typeof window !== "undefined") {
+      getUserDetails();
+    }
   }, [favorites]);
 
   const handleClgClick = (clg) => {
